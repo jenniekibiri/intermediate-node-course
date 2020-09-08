3dc05804446da33d18aaa6da5bca7a66f00f12db
jenniekibiri <josphatje@gmail.com>
2020-09-08 18:28:24 +0300

    create database connection

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
+const dotenv = require('dotenv').config()
 const port=8000;
 const app= express();
-
-app.use(bodyParser.json());
+const User = require('./models/User')
+//create database connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(()=>console.log('database connected'))
+  .catch((err)=>console.log(`error${err}`))
+  app.use(bodyParser.json());
 
 app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
