31c623ba20dd9bc0179945634b1483a444f54a7d
jenniekibiri <josphatje@gmail.com>
2020-09-08 19:08:49 +0300

    create delete post

--- server.js
+++ server.js
@@ -63,9 +63,61 @@ app.route('/users/:id')
 })
 // UPDATE
 .put((req,res)=>{
-  // User.findByIdAndUpdate()
+   const {name,email,password} = req.body
+  User.findByIdAndUpdate(
+
+    req.params.id,
+   
+    {
+     name,
+      email,
+      password
+    },
+    {//returns a modified document
+      new:true
+    },
+    (err,data)=>{
+      if (err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    }
+  )
 })
 // DELETE
+// DELETE
 .delete((req,res)=>{
-  // User.findByIdAndDelete()
+  User.findByIdAndDelete(
+    req.params.id,
+    (err,data)=>{
+      if (err){
+        res.json({
+          success: false,
+          message: err
+        })
+      } else if (!data){
+        res.json({
+          success: false,
+          message: "Not Found"
+        })
+      } else {
+        res.json({
+          success: true,
+          data: data
+        })
+      }
+    }
+  )
 })
